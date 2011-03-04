b8660b0344de18f42a67ca63c045a8280baafbd6
kjellgm <kjell.gunnar.midtlyng@bekk.no>
2011-03-04 08:49:44 +0100

    Commiter siste endring

--- gen/no/bekk/R.java
+++ gen/no/bekk/R.java
@@ -12,9 +12,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int pin=0x7f020001;
     }
     public static final class id {
         public static final int mapView=0x7f050000;
+        public static final int zoom=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/no/bekk/StartActivity.java
+++ src/no/bekk/StartActivity.java
@@ -1,20 +1,97 @@
 package no.bekk;
 
+import java.util.List;
+
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapController;
+import com.google.android.maps.Overlay;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.os.Bundle;
- 
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+
 public class StartActivity extends MapActivity {
     /** Called when the activity is first created. */
-    @Override
+	MapView mapView;
+	MapController mc;
+	GeoPoint p;
+	
+	class MapOverlay extends com.google.android.maps.Overlay
+    {
+        @Override
+        public boolean draw(Canvas canvas, MapView mapView, 
+        boolean shadow, long when) 
+        {
+            super.draw(canvas, mapView, shadow);                   
+ 
+            //---translate the GeoPoint to screen pixels---
+            Point screenPts = new Point();
+            mapView.getProjection().toPixels(p, screenPts);
+ 
+            //---add the marker---
+            Bitmap bmp = BitmapFactory.decodeResource(
+                getResources(), R.drawable.pin);            
+            canvas.drawBitmap(bmp, screenPts.x, screenPts.y-24, null);         
+            return true;
+        }
+        
+        @Override
+        public boolean onTouchEvent(MotionEvent event, MapView mapView) 
+        {   
+            //---when user lifts his finger---
+            if (event.getAction() == 1) {                
+                GeoPoint p = mapView.getProjection().fromPixels(
+                    (int) event.getX(),
+                    (int) event.getY());
+                    Toast.makeText(getBaseContext(), 
+                        p.getLatitudeE6() / 1E6 + "," + 
+                        p.getLongitudeE6() /1E6 , 
+                        Toast.LENGTH_SHORT).show();
+            }                            
+            return false;
+        }        
+    } 
+	
+	@Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.setBuiltInZoomControls(true);
+        
+        //Set start position
+        mc = mapView.getController();
+        String coordinates[] = {"63.4", "10.4"};
+        double lat = Double.parseDouble(coordinates[0]);
+        double lng = Double.parseDouble(coordinates[1]);
+ 
+        p = new GeoPoint(
+            (int) (lat * 1E6), 
+            (int) (lng * 1E6));
+ 
+        mc.animateTo(p);
+        mc.setZoom(17); 
+        
+        //Add pin
+        MapOverlay mapOverlay = new MapOverlay();
+        List<Overlay> listOfOverlays = mapView.getOverlays();
+        listOfOverlays.clear();
+        listOfOverlays.add(mapOverlay);        
+ 
+        mapView.invalidate();
     }
  
     @Override
     protected boolean isRouteDisplayed() {
         return false;
     }
+    
 }
